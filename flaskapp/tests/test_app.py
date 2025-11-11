50d8743feba87f10350304cff51ade68e698823b
ABOAGYE KENNETH DAVID <63665356+kofi527@users.noreply.github.com>
2025-11-11 08:46:25 -0600

    Update test_app.py
    parse JSON  body

--- flaskapp/tests/test_app.py
+++ flaskapp/tests/test_app.py
@@ -1,7 +1,14 @@
 from flaskapp.app import app
+import json
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
+    
     assert response.status_code == 200
+    
+    # Parse JSON body
+    data = json.loads(response.data.decode("utf-8"))
+    
+    # Now assert the message
     assert data["message"] == "Hello from DevOps Pipeline this is a clear message from the devops team!"
