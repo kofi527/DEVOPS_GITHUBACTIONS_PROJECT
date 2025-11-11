ee9ff2b2250e4050fba54aee53ae9f266e4ef5fc
ABOAGYE KENNETH DAVID <63665356+kofi527@users.noreply.github.com>
2025-11-11 08:44:12 -0600

    Update test_app.py
    updated message for testfile

--- flaskapp/tests/test_app.py
+++ flaskapp/tests/test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello, DevOps!" in response.data
+    assert data["message"] == "Hello from DevOps Pipeline this is a clear message from the devops team!"
