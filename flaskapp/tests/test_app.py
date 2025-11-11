edee8f656cc3929c2c4873c86caa1ad547af55dc
ABOAGYE KENNETH DAVID <63665356+kofi527@users.noreply.github.com>
2025-11-11 08:40:35 -0600

    Update test_app.py
    updated project directory

--- flaskapp/tests/test_app.py
+++ flaskapp/tests/test_app.py
@@ -1,6 +1,7 @@
-from flaskapp import app
+from flaskapp.app import app
 
 def test_home():
     client = app.test_client()
-    res = client.get('/')
-    assert res.status_code == 200
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello, DevOps!" in response.data
